de30af8515ef683ccd1dc18d3395c3588cdd4fdf
Aleksandr Dimkov <dimkovaleks4@gmail.com>
2023-10-03 16:46:53 +0300

    feat: add NotFoundPage components test

---
+++ src/pages/NotFoundPage/NotFoundPage.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { fireEvent, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import NotFoundPage from './NotFoundPage';
+import renderPage from '@/utils/tests/utilts';
+
+test('Should navigate to home page', async () => {
+  renderPage(
+    <BrowserRouter>
+      <NotFoundPage />
+    </BrowserRouter>
+  );
+
+  const toAuthPageButton = screen.getByText('back home');
+  fireEvent.click(toAuthPageButton);
+
+  expect(window.location.pathname).toBe('/home');
+});
