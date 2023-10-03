c519d3c301b446fcf0cd8350841f16ea476f0fd7
Aleksandr Dimkov <dimkovaleks4@gmail.com>
2023-10-03 17:02:51 +0300

    feat: add register and login form components

--- src/pages/AuthPage/components/LoginForm.tsx
+++ src/pages/AuthPage/components/LoginForm.tsx
@@ -1,27 +1,72 @@
-import React, { ReactElement, ChangeEvent, FormEvent } from 'react';
+import React, { ReactElement, ChangeEvent, FormEvent, useState, Dispatch, SetStateAction } from 'react';
 import Logo from '@/components/Logo/Logo';
 import styles from '../AuthPage.module.scss';
 import FormRow from '@/components/FormRow/FormRow';
-import { IAuthFormProps } from '@/interfaces/componentIterfaces';
+import NotificationMessages from '@/enums/notificationMessage';
+import { showNotification } from '@/utils/helpers/antd/antdConfig';
+import { loginUser } from '@/store/actions/userActions';
+import { useAppDispatch, useAppSelector } from '@/utils/hooks/redux';
+import { handleInputChange } from '../AuthPage';
+import { checkIsAllInputValid } from '@/utils/helpers/validationSchemes/authSchema';
+import { IAuthFormProps } from '@/interfaces/IComponents';
 
 const LoginForm = ({ inputValues, setInputValues, setIsMember }: IAuthFormProps): ReactElement => {
-  const handleInputChange = (event: ChangeEvent): void => {
-    const input = event.target as HTMLInputElement;
-    const changedField = input.name;
-    setInputValues({ ...inputValues, [changedField]: input.value });
-  };
+  const { email, password } = inputValues;
+  const [validateMessages, setValidateMessages]: [
+    Record<string, string>,
+    Dispatch<SetStateAction<Record<string, string>>>,
+  ] = useState({});
+
+  const isLoading = useAppSelector((store) => store.user.isLoading);
+  const dispatch = useAppDispatch();
 
   const handleSubmit = (event: FormEvent): void => {
     event.preventDefault();
+    const isAllInputValid = checkIsAllInputValid(inputValues, setValidateMessages);
+
+    if (!email || !password) {
+      showNotification(NotificationMessages.EMPTY_AUTH_FIELDS);
+    } else if (!isAllInputValid) {
+      showNotification(NotificationMessages.CHECK_AUTH_FIELDS_VALUE);
+    } else {
+      dispatch(loginUser(inputValues));
+    }
   };
 
   return (
     <form className={styles.form} onSubmit={(event): void => handleSubmit(event)}>
       <Logo className={styles.formLogo} />
       <h3 className={styles.formHeading}>Login</h3>
-      <FormRow type="text" name="email" value={inputValues.email} handleInputChange={handleInputChange} />
-      <FormRow type="password" name="password" value={inputValues.password} handleInputChange={handleInputChange} />
-      <button type="submit" className={`btn btn-block ${styles.btn}`}>
+      <FormRow
+        type="text"
+        name="email"
+        value={email}
+        placeholder="Enter your email*"
+        autoFocus={true}
+        validateMessage={validateMessages.email}
+        handleInputChange={(event: ChangeEvent): void =>
+          handleInputChange(event, inputValues, setInputValues, setValidateMessages)
+        }
+      />
+      <FormRow
+        type="password"
+        name="password"
+        value={password}
+        placeholder="Enter your password*"
+        validateMessage={validateMessages.password}
+        handleInputChange={(event: ChangeEvent): void =>
+          handleInputChange(event, inputValues, setInputValues, setValidateMessages)
+        }
+      />
+      <button
+        className={`btn btn-block ${styles.btn}`}
+        type="submit"
+        disabled={
+          isLoading ||
+          !Object.entries(inputValues).some(([, inputValue]) => !!inputValue) ||
+          Object.entries(validateMessages).some(([, errorMessage]) => !!errorMessage)
+        }
+      >
         submit
       </button>
       <div className={styles.redirectBtnContainer}>
