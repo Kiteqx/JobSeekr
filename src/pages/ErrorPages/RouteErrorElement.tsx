3fced87a190ae616632b497d133e218ccf3a22d0
Aleksandr Dimkov <dimkovaleks4@gmail.com>
2023-10-04 17:03:34 +0300

    feat: add RouteErrorPage component with tests

---
+++ src/pages/ErrorPages/RouteErrorElement.tsx
@@ -0,0 +1,26 @@
+import React, { ReactElement } from 'react';
+import { Link } from 'react-router-dom';
+import errorBoundarySVG from '@/assets/images/svg/route-error-page.svg';
+import styles from './ErrorPages.module.scss';
+
+const RouteErrorElement = (): ReactElement => {
+  return (
+    <main className={`container ${styles.main}`}>
+      <div>
+        <img className={styles.notFoundImg} src={errorBoundarySVG} alt="error boundary image" />
+        <h3 className={styles.heading}>Oops! Error has occurred.</h3>
+        <p className={styles.description}>
+          We are working to fix it. Please reload a page or
+          <a className={styles.supportLink} href="https://github.com/DiegoKitty/JobSeekr/issues" target="_blank">
+            contact our support.
+          </a>
+        </p>
+        <Link className={`btn ${styles.toHomeLink}`} to="/home">
+          back home
+        </Link>
+      </div>
+    </main>
+  );
+};
+
+export default RouteErrorElement;
---
+++ src/pages/ErrorPages/tests/RouteErrorPage.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { fireEvent, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import RouteErrorElement from '../RouteErrorElement';
+import renderPage from '@/utils/tests/utilts';
+
+test('Should navigate to home page', async () => {
+  renderPage(
+    <BrowserRouter>
+      <RouteErrorElement />
+    </BrowserRouter>
+  );
+
+  const toAuthPageButton = screen.getByText('back home');
+  fireEvent.click(toAuthPageButton);
+
+  expect(window.location.pathname).toBe('/home');
+});
