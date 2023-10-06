250556ca64b2302d65625a1e990254b843dd2a34
Aleksandr Dimkov <dimkovaleks4@gmail.com>
2023-10-06 15:20:02 +0300

    feat: update redux IUserState interface

--- src/interfaces/IRedux.tsx
+++ src/interfaces/IRedux.tsx
@@ -2,5 +2,6 @@ import { IResponseUserData } from './IAPI';
 
 export interface IUserState {
   user: Pick<IResponseUserData['data'], 'user'>['user'] | null;
+  isSidebarOpen: boolean;
   isLoading: boolean;
 }
