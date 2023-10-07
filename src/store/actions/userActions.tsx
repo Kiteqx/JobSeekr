a43cb20943672b3918bfaa2d7610f31cbebbf789
Aleksandr Dimkov <dimkovaleks4@gmail.com>
2023-10-07 22:46:05 +0300

    feat: update user's slice and actions

--- src/store/actions/userActions.tsx
+++ src/store/actions/userActions.tsx
@@ -2,8 +2,13 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import APIEndpoints from '@/enums/APIEndpoints';
 import { IResponseUserData, IResponseError } from '@/interfaces/IAPI';
+import { logoutUser } from '../reducers/userSlice';
+import errorStatusCodes from '@/enums/errorStatusCodes';
+import { TThunkApiConfig } from '@/types/TRedux';
 
-export const registerUser = createAsyncThunk(
+const createUserAsyncThunk = createAsyncThunk.withTypes<TThunkApiConfig>();
+
+export const registerUser = createUserAsyncThunk(
   'user/registerUser',
   async (inputValues: Record<string, string>, thunkAPI) => {
     try {
@@ -20,16 +25,47 @@ export const registerUser = createAsyncThunk(
   }
 );
 
-export const loginUser = createAsyncThunk('user/loginUser', async (inputValues: Record<string, string>, thunkAPI) => {
-  try {
-    const {
-      data: { user },
-    }: IResponseUserData = await axios.post(APIEndpoints.URL_LOGIN, inputValues);
-    return thunkAPI.fulfillWithValue(user);
-  } catch (e) {
-    const {
-      response: { data },
-    } = e as IResponseError;
-    return typeof data === 'object' ? thunkAPI.rejectWithValue(data.msg) : thunkAPI.rejectWithValue(data);
+export const loginUser = createUserAsyncThunk(
+  'user/loginUser',
+  async (inputValues: Record<string, string>, thunkAPI) => {
+    try {
+      const {
+        data: { user },
+      }: IResponseUserData = await axios.post(APIEndpoints.URL_LOGIN, inputValues);
+      return thunkAPI.fulfillWithValue(user);
+    } catch (e) {
+      const {
+        response: { data },
+      } = e as IResponseError;
+      return typeof data === 'object' ? thunkAPI.rejectWithValue(data.msg) : thunkAPI.rejectWithValue(data);
+    }
   }
-});
+);
+
+export const updateUser = createUserAsyncThunk(
+  'user/updateUser',
+  async (inputValues: Record<string, string>, thunkAPI) => {
+    try {
+      const userState = thunkAPI.getState().user.user;
+      const {
+        data: { user },
+      }: IResponseUserData = await axios.patch(APIEndpoints.URL_UPDATE_USER, inputValues, {
+        headers: {
+          authorization: `Bearer ${userState?.token || ''}`,
+        },
+      });
+
+      return thunkAPI.fulfillWithValue(user);
+    } catch (e) {
+      const {
+        response: { status: statusCode, data },
+      } = e as IResponseError;
+
+      if (statusCode === errorStatusCodes.UNAUTHORIZER) {
+        thunkAPI.dispatch(logoutUser());
+        return thunkAPI.rejectWithValue('Unauthorized! Logging Out...');
+      }
+      return typeof data === 'object' ? thunkAPI.rejectWithValue(data.msg) : thunkAPI.rejectWithValue(data);
+    }
+  }
+);
