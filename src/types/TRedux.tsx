eab4226d8b7c1ac009913c100348e48d8dec99f8
Aleksandr Dimkov <dimkovaleks4@gmail.com>
2023-10-07 22:47:31 +0300

    feat: add TThunkApiConfig type

--- src/types/TRedux.tsx
+++ src/types/TRedux.tsx
@@ -3,3 +3,10 @@ import { store, rootReducer } from '@/store/store';
 export type TRootState = ReturnType<typeof rootReducer>;
 export type TAppStore = typeof store;
 export type TAppDispatch = TAppStore['dispatch'];
+
+export type TThunkApiConfig = {
+  state: TRootState;
+  dispatch: TAppDispatch;
+  rejectValue: unknown;
+  extra: unknown;
+};
