8b58548d72ed875e18e0cf9675588fbce64fe1e1
Aleksandr Dimkov <dimkovaleks4@gmail.com>
2023-10-15 07:04:38 +0300

    feat: update AddJobPage component

--- src/pages/AddJobPage/AddJobPage.tsx
+++ src/pages/AddJobPage/AddJobPage.tsx
@@ -3,8 +3,9 @@ import styles from './AddJobPage.module.scss';
 import FormRow from '@/components/FormRow/FormRow';
 import FormRowSelect from '@/components/FormRowSelect/FormRowSelect';
 import { useAppDispatch, useAppSelector } from '@/utils/hooks/redux';
-import { handleChangeState, handleResetState } from '@/store/reducers/jobSlice';
-import { createJob } from '@/store/actions/jobActions';
+import { handleChangeState, clearAddJobState } from '@/store/reducers/jobSlice';
+import { createJob, editJob } from '@/store/actions/jobActions';
+import Preloader from '@/components/Preloader/Preloader';
 
 const AddJobPage = (): ReactElement => {
   const {
@@ -17,7 +18,7 @@ const AddJobPage = (): ReactElement => {
     status,
     statusOptions,
     isEditing,
-    // editJobId,
+    editJobId,
   } = useAppSelector((store) => store.job);
   const dispatch = useAppDispatch();
 
@@ -28,69 +29,80 @@ const AddJobPage = (): ReactElement => {
   };
 
   const handleResetJobState = (): void => {
-    dispatch(handleResetState());
+    dispatch(clearAddJobState());
   };
 
   const handleSubmit = (event: FormEvent): void => {
     event.preventDefault();
+
+    if (isEditing) {
+      dispatch(editJob({ jobId: editJobId, jobData: { position, company, jobLocation, jobType, status } }));
+      return;
+    }
+
     dispatch(createJob({ position, company, jobLocation, jobType, status }));
   };
 
   return (
-    <section className={styles.section}>
-      <form className={`form ${styles.form}`}>
-        <h3>{isEditing ? 'edit job' : 'add job'}</h3>
-        <div className={styles.formCenter}>
-          <FormRow
-            type="text"
-            name="position"
-            value={position}
-            placeholder="Enter position*"
-            onInputChange={handleChangeJobState}
-          />
-          <FormRow
-            type="text"
-            name="company"
-            value={company}
-            placeholder="Enter company*"
-            onInputChange={handleChangeJobState}
-          />
-          <FormRow
-            type="text"
-            labelText="job location"
-            name="jobLocation"
-            value={jobLocation}
-            placeholder="Enter job location*"
-            onInputChange={handleChangeJobState}
-          />
-
-          <FormRowSelect
-            name="status"
-            value={status}
-            labelText="status"
-            list={statusOptions}
-            onChange={handleChangeJobState}
-          />
-
-          <FormRowSelect
-            name="jobType"
-            labelText="job type"
-            value={jobType}
-            list={jobTypeOptions}
-            onChange={handleChangeJobState}
-          />
-
-          <div className={styles.btnContainer}>
-            <button type="button" className={`btn btn-block ${styles.clearBtn}`} onClick={handleResetJobState}>
-              clear
-            </button>
-            <button type="submit" className={`btn btn-block`} disabled={isLoading} onClick={handleSubmit}>
-              submit
-            </button>
+    <>
+      {isLoading && <Preloader containerClassname="cornerLoaderContainer" />}
+      <section className={styles.section}>
+        <form className={`form ${styles.form}`}>
+          <h3>{isEditing ? 'edit job' : 'add job'}</h3>
+          <div className={styles.formCenter}>
+            <FormRow
+              type="text"
+              name="position"
+              value={position}
+              placeholder="Enter position*"
+              onInputChange={handleChangeJobState}
+            />
+            <FormRow
+              type="text"
+              name="company"
+              value={company}
+              placeholder="Enter company*"
+              onInputChange={handleChangeJobState}
+            />
+            <FormRow
+              type="text"
+              labelText="job location"
+              name="jobLocation"
+              value={jobLocation}
+              placeholder="Enter job location*"
+              onInputChange={handleChangeJobState}
+            />
+            <FormRowSelect
+              name="status"
+              value={status}
+              labelText="status"
+              list={statusOptions}
+              onChange={handleChangeJobState}
+            />
+            <FormRowSelect
+              name="jobType"
+              labelText="job type"
+              value={jobType}
+              list={jobTypeOptions}
+              onChange={handleChangeJobState}
+            />
+            <div className={styles.btnContainer}>
+              <button type="button" className={`btn btn-block ${styles.clearBtn}`} onClick={handleResetJobState}>
+                clear
+              </button>
+              <button
+                type="submit"
+                className={`btn btn-block`}
+                disabled={isLoading || !position || !company || !jobLocation}
+                onClick={handleSubmit}
+              >
+                {isLoading ? 'please wait...' : 'submit'}
+              </button>
+            </div>
           </div>
-        </div>
-      </form>
-    </section>
+        </form>
+      </section>
+    </>
   );
 };
 
